bcf1df4b280230f2b1038cf730801fa6c7cfca57
Douglas Rogers <doug@indieambassador.com>
2012-05-31 13:23:15 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cryosearch::Application.config.secret_token = 'd3e81b8412040146d12440ffb0bd6b19e22d07127e96b43d810cf177b1c3f5712be8b3388a1facdcfc4052e1089d609038a02343c22b93698f954a07c0e96be4'

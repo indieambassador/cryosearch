b19b6860e78cfaa88baaa2cf19eafe0fe856d082
Douglas Rogers <doug@indieambassador.com>
2012-06-13 00:42:29 -0400

    updated the add a protocol thing and view

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,5 +1,5 @@
 class Protocol < ActiveRecord::Base
-  attr_accessible :cell_or_tissue_type, :description, :genus, :species, :user_id
+  attr_accessible :cell_or_tissue_type, :description, :genus, :species, :user_id, :materials, :references, :instructions
 
   belongs_to :user
 end

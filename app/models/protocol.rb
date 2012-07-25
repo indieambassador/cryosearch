5fea2240abc3cf101851718373834268d201ecae
Douglas Rogers <doug@indieambassador.com>
2012-07-25 19:56:03 -0400

    some formatting and wysiwyg

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,5 +1,8 @@
 class Protocol < ActiveRecord::Base
   attr_accessible :cell_or_tissue_type, :title, :description, :genus, :species, :user_id, :materials, :references, :instructions
+  
+  validates :cell_or_tissue_type, :title, :description, :genus, :species, :user_id, :materials, :references, :instructions, :presence => true
+
 
   belongs_to :user
 end
